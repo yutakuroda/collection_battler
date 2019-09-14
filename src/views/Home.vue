2391c5cdb15452d603a9e4be4cf3d2ca0f4f874b
yuta <yuta@yuta.kuroda>
2019-09-14 14:54:25 +0900

    add button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Logo msg="収蔵品バトラー"/>
     <TextInput title_text="検索キーワード"/>
+    <ButtonGreen text="Go!"/>
   </div>
 </template>
 
@@ -9,12 +10,13 @@
 // @ is an alias to /src
 import Logo from '@/components/Logo.vue'
 import TextInput from '@/components/TextInput.vue'
-
+import ButtonGreen from '@/components/ButtonGreen.vue'
 export default {
   name: 'home',
   components: {
     Logo,
-    TextInput
+    TextInput,
+    ButtonGreen
   }
 }
 </script>
