4afb2e11eadeb85e41f87e489aa81808198ca836
yuta <yuta@yuta.kuroda>
2019-09-15 10:08:38 +0900

    画像を選択できるようにした

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,24 @@
     <TextInput title_text="検索キーワード"/>
     <SearchButton text="検索する"/>
     <ButtonGreen text="Go!"/>
+<<<<<<< Updated upstream
     <BattlerIconSet url="https://colbase.nich.go.jp/uploads/collection_item_images/thumbnail/3c044d87c6b4a243aff2b469d72c8495.jpg" icon_title="持統天皇"/>
+=======
+    <ul id="v-for-object" class="demo">
+      <li v-for="value in collections">
+        <input :id="value.id" type="radio" name="rate" value="a">
+          <label :for="value.id">
+        <!-- {{ value }} -->
+          <BattlerIconSet :url="value.common.thumbnailUrl" :icon_title="value.common.title" />
+        </label>
+        <!-- <p>{{value.common}}</p> -->
+        <!-- <BattlerIconSet url="http://dl.ndl.go.jp/titleThumb/info:ndljp/pid/2540649" icon_title="ほげ"/> -->
+      </li>
+    </ul>
+
+    <!-- <BattlerIconSet icon_title="持統天皇"/> -->
+    <!-- <BattlerIconSet url="https://colbase.nich.go.jp/uploads/collection_item_images/thumbnail/3c044d87c6b4a243aff2b469d72c8495.jpg" icon_title="持統天皇"/> -->
+>>>>>>> Stashed changes
  </div>
 </template>
 
@@ -26,3 +43,12 @@ export default {
   },
 }
 </script>
+<style>
+input[type="radio"]:checked + label {
+  background: orange;
+  color: blue;
+}
+input[type="radio"] {
+  display: none;
+}
+</style>
